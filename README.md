06e03e5d9458676448ef0e38ee314271b3938a35
Han Wu <wuhanstudio@qq.com>
2025-03-07 12:23:34 +0000

    [Project 1] hello-vkinfo

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
 # Vulkan Compute Shader
 
+## Windows Users
+
+Install Visual Studio and open `vulkan-compute-shader.sln`.
+
+## Prerequisites (Linux)
+
+Install vcpkg:
+
+```
+$ sudo apt install build-essential pkg-config cmake curl zip unzip tar ninja-build
+$ sudo apt install libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev
+$ sudo apt install libxrandr-dev libxi-dev
+
+$ git clone https://github.com/microsoft/vcpkg.git
+$ cd vcpkg
+$ ./bootstrap-vcpkg.sh
+$ echo 'export VCPKG_ROOT=$HOME/vcpkg' >> ~/.bashrc
+$ echo 'export PATH=$PATH:$VCPKG_ROOT' >> ~/.bashrc
+$ source ~/.bashrc
+$ vcpkg integrate install
+```
+
+Install vulkan-validationlayers for debugging:
+
 ```
 $ sudo apt install vulkan-validationlayers
+```
+
+## Project 1: hello-vkinfo
+
+```
+$ cd hello-vkinfo
+$ cmake -B build --preset vcpkg
+$ cmake --build build
+$ ./build/hello-vkinfo
+```
+
+Output:
+
+```
+Vulkan API version: 1.3.296
+
+Number of physical devices: 2
+
+Available devices:
+Device name: Intel(R) Arc(TM) Graphics
+Device type: Integrated GPU
+API version: 1.3.277
+
+Device name: NVIDIA RTX 1000 Ada Generation Laptop GPU
+Device type: Discrete GPU
+API version: 1.3.277
 ```
--- hello-vkinfo/main.cpp
+++ hello-vkinfo/main.cpp
@@ -3,9 +3,31 @@
 #include <fmt/core.h>
 #include <vector>
 
+const char* vkPhysicalDeviceType_as_string(VkPhysicalDeviceType type) {
+	switch (type)
+	{
+	case VK_PHYSICAL_DEVICE_TYPE_OTHER:
+		return "Other";
+
+	case VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU:
+		return "Integrated GPU";
+
+	case VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU:
+		return "Discrete GPU";
+
+	case VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU:
+		return "Virtualized GPU";
+
+	case VK_PHYSICAL_DEVICE_TYPE_CPU:
+		return "CPU";
+
+	default:
+		return "Unknown";
+	}
+}
+
 int main() {
 	// Initialize Vulkan
-	VkInstance instance;
 	VkApplicationInfo appInfo = {};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	appInfo.pApplicationName = "Hello Vulkan";
@@ -18,9 +40,28 @@ int main() {
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
 
+	// Create a Vulkan instance
+	VkInstance instance;
 	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
 		fmt::print("Failed to create Vulkan instance\n");
 		return -1;
+	}
+
+	// Print the Vulkan version
+	uint32_t apiVersion;
+	auto FN_vkEnumerateInstanceVersion = PFN_vkEnumerateInstanceVersion(vkGetInstanceProcAddr(nullptr, "vkEnumerateInstanceVersion"));
+
+	if (FN_vkEnumerateInstanceVersion == nullptr)
+	{
+		fmt::println("vkEnumerateInstanceVersion is not available. Using VK_API_VERSION_1_0\n");
+	}
+	else
+	{
+		vkEnumerateInstanceVersion(&apiVersion);
+		fmt::println("Vulkan API version: {}.{}.{}\n",
+			VK_VERSION_MAJOR(apiVersion),
+			VK_VERSION_MINOR(apiVersion),
+			VK_VERSION_PATCH(apiVersion));
 	}
 
 	// Query the number of Vulkan physical devices
@@ -39,6 +80,11 @@ int main() {
 		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
 		fmt::println("Device name: {}", deviceProperties.deviceName);
+		fmt::println("Device type: {}", vkPhysicalDeviceType_as_string(deviceProperties.deviceType));
+		fmt::println("API version: {}.{}.{}\n",
+			VK_VERSION_MAJOR(deviceProperties.apiVersion),
+			VK_VERSION_MINOR(deviceProperties.apiVersion),
+			VK_VERSION_PATCH(deviceProperties.apiVersion));
 	}
 
 	// Destroy the Vulkan instance
